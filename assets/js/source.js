3390ecfec6ac37e3dce28b4c408f9f8540b67b32
syy11cn <syy11cn@outlook.com>
2021-12-13 15:50:22 +0800

    improve user experience

--- assets/js/source.js
+++ assets/js/source.js
@@ -4,7 +4,17 @@ const YiyangSun = {
   introduction: "Hi, there 👋! I'm Yiyang Sun. Currently student of UESTC. Frontend developer.",
   blog: "https://blog.syy11.cn/",
   github: "https://github.com/syy11cn/",
-  twitter: "https://zhihu.com/people/syy11cn/"
+  twitter: "https://zhihu.com/people/syy11cn/",
+  works: [
+    {
+      name: "hexo-theme-linear",
+      repo: "https://github.com/syy11cn/hexo-theme-linear/",
+    },
+    {
+      name: "config-router",
+      repo: "https://github.com/syy11cn/config-router/",
+    },
+  ],
 };
 \`\`\`
 `;
